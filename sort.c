ae4aa47b74e487b73a9003313408628ca7393722
Jonathan-Snyder1 <jonathan@flsnyder.com>
2023-04-09 20:19:31 -0400

    heapify, heap

--- sort.c
+++ sort.c
@@ -1,20 +1,112 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 
 int extraMemoryAllocated;
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+void heapify(int arr[], int n, int i)
+{
+    int biggest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[biggest])
+        biggest = l;
+
+    if (r < n && arr[r] > arr[biggest])
+        biggest = r;
+
+    if (biggest != i) {
+        swap(&arr[i], &arr[biggest]);
+        heapify(arr, n, biggest);
+    }
+}
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+    int i;
+    extraMemoryAllocated = sizeof(int) * n;
+    for (i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+
+        heapify(arr, i, 0);
+    }
 }
 
 
-// implement merge sort
-// extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int* L = (int*) malloc(sizeof(int) * n1);
+    int* R = (int*) malloc(sizeof(int) * n2);
+
+    extraMemoryAllocated += sizeof(int) * (n1 + n2);
+
+    for (i = 0; i < n1; i++)
+        L[i] = pData[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = pData[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            pData[k] = L[i];
+            i++;
+        } else {
+            pData[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        pData[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        pData[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
+}
+
+
+//implement merge sort
+//extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+    if (l < r) {
+        int m = (l+r)/2;
+
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+
+        merge(pData, l, m, r);
+    }
 }
 
 // parses input file to an integer array
@@ -24,7 +116,7 @@ int parseData(char *inputFileName, int **ppData)
 	int dataSz = 0;
 	int i, n, *data;
 	*ppData = NULL;
-	
+
 	if (inFile)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
@@ -44,7 +136,7 @@ int parseData(char *inputFileName, int **ppData)
 
 		fclose(inFile);
 	}
-	
+
 	return dataSz;
 }
 
@@ -58,7 +150,7 @@ void printArray(int pData[], int dataSz)
 		printf("%d ",pData[i]);
 	}
 	printf("\n\t");
-	
+
 	for (i=sz;i<dataSz;++i)
 	{
 		printf("%d ",pData[i]);
@@ -72,21 +164,21 @@ int main(void)
 	int i;
     double cpu_time_used;
 	char* fileNames[] = { "input1.txt", "input2.txt", "input3.txt", "input4.txt" };
-	
+
 	for (i=0;i<4;++i)
 	{
 		int *pDataSrc, *pDataCopy;
 		int dataSz = parseData(fileNames[i], &pDataSrc);
-		
+
 		if (dataSz <= 0)
 			continue;
-		
+
 		pDataCopy = (int *)malloc(sizeof(int)*dataSz);
-	
+
 		printf("---------------------------\n");
 		printf("Dataset Size : %d\n",dataSz);
 		printf("---------------------------\n");
-		
+
 		printf("Heap Sort:\n");
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
@@ -97,7 +189,7 @@ int main(void)
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		
+
 		printf("Merge Sort:\n");
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
@@ -108,9 +200,9 @@ int main(void)
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		
+
 		free(pDataCopy);
 		free(pDataSrc);
 	}
-	
+
 }
